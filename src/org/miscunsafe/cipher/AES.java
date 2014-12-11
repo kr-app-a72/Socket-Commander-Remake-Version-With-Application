75b9a8db9b0282a2437d4d4952f7397c5a9eae72
yourmama397 <peb3978@naver.com>
2014-12-11 17:26:45 -0500

    ...

--- src/org/miscunsafe/cipher/AES.java
+++ src/org/miscunsafe/cipher/AES.java
@@ -291,24 +291,11 @@ public final class AES extends Cipher {
 
 		return retc;
 	}
-
+/*
 	// Used for test.
 	public static void main (String ... args) throws Throwable {
-		AES ___this = new AES ();
-		
-		___this.setKey ("TESTTESTTESTTEST");
-
-		char test [] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-
-		System.out.println (test);
-		mixColumns (___this, test);
-		
-		System.out.println (test);
-		
-		invMixColumns (___this, test);
-		System.out.println (test);
 	}
-
+*/
 	@Override
 	public boolean encode (char data [], char result []) throws CipherException {
 		return true;
