eb5d1deb9dea344bb6e508785ad2a9daf168ae47
Eunbin Park <yourmama397@users.noreply.github.com>
2014-12-12 18:50:06 +0000

    .

--- src/org/miscunsafe/cipher/AES.java
+++ src/org/miscunsafe/cipher/AES.java
@@ -200,97 +200,21 @@ public final class AES extends Cipher {
 		return (data >= 0x20 && data < 0x7F) || data == '+' || data == '/';
 	}
 
-	protected static String base64_encode (char data []) {
-		char arr_3 [] = new char [3], arr_4 [] = new char [4];
-		int i = 0, j = 0, k = 0, len = data.length;
-		StringBuilder ret = new StringBuilder ();
-
-		while ((len --) != 0) {
-			arr_3 [i ++] = data [k ++];
-
-			if (i == 3) {
-				arr_4 [0] = (char) ((arr_3 [0] & 0xFC) >> 2);
-				arr_4 [1] = (char) (((arr_3 [0] & 0x03) << 4) + ((arr_3 [1] & 0xF0) >> 4));
-				arr_4 [2] = (char) (((arr_3 [1] & 0x0F) << 2) + ((arr_3 [2] & 0xC0) >> 6));
-				arr_4 [3] = (char) (arr_3 [2] & 0x3F);
-
-				for (i = 0 ; i < 4 ; i ++)
-					ret.append (base64_chars.charAt(arr_4 [i]));
-
-				i = 0;
-			}
-		}
-
-		if (i != 0) { // If need to put extra character.
-			for (j = i ; j < 3 ; j ++)
-				arr_3 [j] = 0;
-
-			arr_4 [0] = (char) ((arr_3 [0] & 0xFC) >> 2);
-			arr_4 [1] = (char) (((arr_3 [0] & 0x03) << 4) + ((arr_3 [1] & 0xF0) >> 4));
-			arr_4 [2] = (char) (((arr_3 [1] & 0x0F) << 2) + ((arr_3 [2] & 0xC0) >> 6));
-			arr_4 [3] = (char) (arr_3 [2] & 0x3F);
-
-			for (j = 0 ; j < (i + 1) ; j ++)
-				ret.append (base64_chars.charAt (arr_4 [j]));
-
-			while (i ++ < 3)
-				ret.append ('=');
-		}
-
-		return ret.toString ();
-	}
-
-	protected static char [] base64_decode (String str) {
-		char arr_3 [] = new char [3], arr_4 [] = new char [4], retc [];
-		int i = 0, j = 0, k = 0, len = str.length ();
-		StringBuilder ret = new StringBuilder ();
-
-		while (len != 0 && str.charAt (k) != '=' && isBase64 (str.charAt (k))) {
-			arr_4 [i ++] = str.charAt (k ++);
-			len --;
-
-			if (i == 4) {
-				for (i = 0 ; i < 4 ; i ++)
-					arr_4 [i] = (char) base64_chars.indexOf (arr_4 [i]);
-
-				arr_3 [0] = (char) ((arr_4 [0] << 2) + ((arr_4 [1] & 0x30) >> 4));
-				arr_3 [1] = (char) (((arr_4 [1] & 0x0F) << 4) + ((arr_4 [2] & 0x3C) >> 2));
-				arr_3 [2] = (char) (((arr_4 [2] & 0x03) << 6) + arr_4 [3]);
-
-				for (i = 0 ; i < 3 ; i ++)
-					ret.append (arr_3 [i]);
-
-				i = 0;
-			}
-		}
-
-		if (i != 0) { // left overs.
-			for (j = i ; j < 4 ; j ++)
-				arr_4 [j] = 0;
-
-			for (j = 0 ; j < 4 ; j ++)
-				arr_4 [j] = (char) base64_chars.indexOf (arr_4 [j]);
-
-			arr_3 [0] = (char) ((arr_4 [0] << 2) + ((arr_4 [1] & 0x30) >> 4));
-			arr_3 [1] = (char) (((arr_4 [1] & 0x0F) << 4) + ((arr_4 [2] & 0x3C) >> 2));
-			arr_3 [2] = (char) (((arr_4 [2] & 0x03) << 6) + arr_4 [3]);
-
-			for (j = 0 ; j < (i - 1) ; j ++) 
-				ret.append (arr_3 [j]);
-		}
-		
-		retc = new char [ret.toString ().length ()];
-		ret.toString ().getChars(0, ret.toString ().length (), retc, 0);
-
-		return retc;
-	}
 /*
 	// Used for test.
 	public static void main (String ... args) throws Throwable {
 	}
 */
 	@Override
-	public String encode (char data []) throws CipherException { return null;
+	public String encode (char data []) throws CipherException {
+		Boost boost = new Boost ();
+		StringBuilder ret = new StringBuilder ();
+		String buffer_s;
+		char buffer [] = new char [data.length ()];
+		
+		System.arraycopy (data, 0, buffer, 0, buffer.length);
+		
+		return ret.toString ();
 	}
 
 	@Override
