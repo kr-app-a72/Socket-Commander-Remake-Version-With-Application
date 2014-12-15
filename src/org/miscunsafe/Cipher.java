aab9ce51c882bad451395d00b0bbb3ccbbd55823
Eunbin Park <yourmama397@users.noreply.github.com>
2014-12-14 22:59:48 -0500

    Added javadoc. Might sounded retarded.

--- src/org/miscunsafe/Cipher.java
+++ src/org/miscunsafe/Cipher.java
@@ -31,6 +31,13 @@ public strictfp abstract class Cipher extends Object {
 	protected Cipher () {
 	}
 
+	/**
+	 * Creates new cipher instance.
+	 * @return Cipher Instance of Cipher.
+	 * @param str "Nickname" of cipher class.
+	 * @throw CipherException This exception will occur when following cipher name does not match any of 
+	 * instances of cipher class.
+	 */
 	public static final Cipher getCipher (String str) throws CipherException {
 		if (str.equalsIgnoreCase ("JAES"))
 			return new JavaAES ();
@@ -38,38 +45,86 @@ public strictfp abstract class Cipher extends Object {
 		throw new CipherException ("Unknown cipher name.");
 	}
 
+	/**
+	 * Encodes data.
+	 * @param data Data to encode.
+	 * @param result Result of data will be stored into this variable. This array length should be longer than data array.
+	 * @throw CipherException This exception will occur when there was an error.
+	 * @see org.miscunsafe.Cipher#decode(char [], char []);
+	 */
 	public void encode (char data [], char result []) throws CipherException {
 	}
 
+	/**
+	 * Decodes data.
+	 * @param data Data to decode.
+	 * @param result Result of data will be stored into this variable. This array length should be longer than data array.
+	 * @throw CipherException This exception will occur when there was an error.
+	 * @see org.miscunsafe.Cipher#encode(char [], char []);
+	 */
 	public void decode (char data [], char result []) throws CipherException {
 	}
 	
+	/**
+	 * Encodes data.
+	 * This method will return result of encryption with BASE64 encoding.
+	 * @param data Data to encode.
+	 * @return String result of encoding. Will be encoded with base64
+	 * @throw CipherException This exception will occur if there was an error.
+	 */
 	public String encode (char data []) throws CipherException { return null;
 	}
 
+	/**
+	 * Decodes data.
+	 * This method will return result of encryption with BASE64 format.
+	 * @param str String to decode. Must be in base64 format.
+	 * @return char [] Result.
+	 * @throw CipherException This exception will occur if there was an error.
+	 */
 	public char [] decode (String str) throws CipherException { return null;
 	}
 
+	/**
+	 * Sets key for current cipher.
+	 * @param key Key value.
+	 * @throw CipherException This exception will be thrown if the key is not eligible for this cipher.
+	 */
 	public abstract void setKey (char key []) throws CipherException;
 
-	public void encodeString (String str) throws CipherException {
-		char data [] = new char [str.length ()], extra [] = new char [str.length ()];
+	/**
+	 * Encodes string.
+	 * Basically, this method converts string to char array, then try to encode the data.
+	 * @param str String to encode.
+	 * @throw CipherException Will be thrown when encoding fails.
+	 * @return String Result of encoding. 
+	 */
+	public String encodeString (String str) throws CipherException {
+		char data [] = new char [str.length ()];
 
 		str.getChars (0, str.length (), data, 0);
-
-		encode (data, extra);
-		System.arraycopy (extra, 0, data, 0, str.length ());
+		return encode (data);
 	}
 
-	public void decodeString (String str) throws CipherException {
-		char data [] = new char [str.length ()], extra [] = new char [str.length ()];
+	/**
+	 * Decodes string.
+	 * Basically, this method converts string to char array, then try to decode the data.
+	 * @param str String to decode.
+	 * @throw CipherException Will be thrown when decoding fails.
+	 * @return String Result of decoding. 
+	 */
+	public String decodeString (String str) throws CipherException {
+		char data [] = new char [str.length ()];
 
 		str.getChars (0, str.length (), data, 0);
-
-		decode (data, extra);
-		System.arraycopy (extra, 0, data, 0, str.length ());
+		return decode (data);
 	}
 
+	/**
+	 * Sets key for current cipher.
+	 * @param str Key vlaue.
+	 * @throw CipherException Will be thrown when your key sucks.
+	 */
 	public void setKey (String str) throws CipherException {
 		char data [] = new char [str.length ()];
 
